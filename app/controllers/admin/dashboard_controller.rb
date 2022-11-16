f1dc47c115835e06ea6ddebc54d010cca7ac1a4a
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-11-16 21:15:11 +0000

    Include basic http auth for admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_SECRET']
+  
   def show
   end
 end
