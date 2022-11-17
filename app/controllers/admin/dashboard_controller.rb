543b777429dbfde7dc6262701cc37fc0d555dfd1
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-11-17 20:22:22 +0000

    Include Categories section for admins

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,10 +1,10 @@
 class Admin::DashboardController < ApplicationController
   
   http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_SECRET']
-  
+
   def show
     @product_count = Product.count
-    @category_count = Product.group(:category_id).count.count
+    @category_count = Category.count
   end
-  
+
 end
