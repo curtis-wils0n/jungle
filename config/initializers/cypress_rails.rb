02b8a490e4ad95b7c92db352e3880f13ab95c370
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-11-22 18:54:28 +0000

    Create simple tests for loading home page

--- config/initializers/cypress_rails.rb
+++ config/initializers/cypress_rails.rb
@@ -4,7 +4,15 @@ DatabaseCleaner.strategy = :truncation
 return unless Rails.env.test?
 
 CypressRails.hooks.before_server_start do
-  # Called once, before either the transaction or the server is started
+  cat1 = Category.find_or_create_by! name: 'Evergreens'
+
+  cat1.products.create!({
+    name: 'Giant Tea',
+    description: 'The Giant Tea is an uncommon, medium-sized plant and can be found only in some tundras. It blooms twice a year, for 3 weeks.',
+    image: open_asset('plante_2.jpg'),
+    quantity: 10,
+    price: 24.99
+  })
 end
 
 CypressRails.hooks.after_transaction_start do
@@ -17,4 +25,8 @@ end
 
 CypressRails.hooks.before_server_stop do
   # Called once, at_exit
+end
+
+def open_asset(file_name)
+  File.open(Rails.root.join('db', 'seed_assets', file_name))
 end
---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+describe('Jungle Home Page', () => {
+  
+  it ('retrieves the home page', () => {
+    cy.visit('/');
+  });
+  
+  it('There is a product on the page', () => {
+    cy.get('.products article').should('be.visible');
+  });
+
+  it('There are 2 products on the page', () => {
+    cy.get('.products article').should('have.length', 2);
+  });
+
+})
